888caa6cc751b53614101c45a965ea7feea95f36
oreo <oreo@sparcs.org>
2024-05-09 10:45:53 +0900

    update Main page

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./css/Header.css"
+import logo from "./logo300.png";
 
 const Header = () => {
     return (
         <header>
+            <img src={logo} alt="logo" className="logo"></img>
+            <ul className="navbar">
+                <li className="nav-item">내 만남</li>
+                <li className="nav-item">만남 찾기</li>
+                <li className="nav-item">프로필</li>
+                <li className="nav-item">로그인</li>
+            </ul>
         </header>
     );
 }
