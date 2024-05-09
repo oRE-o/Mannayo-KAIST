8641822f1659ed52422a23fbaf9e616ff3e40db9
oreo <oreo@sparcs.org>
2024-05-09 11:19:58 +0900

    Add: Footer

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import './reset.css';
 import Header from './components/Header.js';
+import Footer from './components/Footer.js';
 
 const App = () => {
   return (
@@ -9,12 +10,13 @@ const App = () => {
     <div className='main-title'>
       <div className='space-200px'></div>
       <div className='title'>Enjoying Involved,</div>
-      <div className='title'>Mannayo!</div>
+      <div className='title'>ManNayo!</div>
       <div className='space-10px'></div>
       <div className='subtitle'>꿈꾸던 만남의 장소는 이미 이 곳에.</div>
       <div className='subtitle'>숨겨진 KAISTian들을 이어주는, 만나요 프로젝트입니다.</div>
-      
     </div>
+    <div className='space-300px'></div>
+    <Footer />
     </>
   );
 }
---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./css/Footer.css"
+
+const Footer = () => {
+    return (
+        <footer>
+            <div className="footer-text">SPARCS Newbie Project Demo - Mannayo</div>
+            <div className="footer-text">Powered with チョテン by oreo@sparcs.org</div>
+        </footer>
+    );
+}
+
+export default Footer;
