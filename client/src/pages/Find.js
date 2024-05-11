c29073eaa273dde93c36c67369f35db6938c0767
oreo <oreo@sparcs.org>
2024-05-11 21:16:14 +0900

    Add: Search-bar to /find

--- client/src/pages/Find.js
+++ client/src/pages/Find.js
@@ -1,6 +1,7 @@
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import MeetingCard from "../components/MeetingCard";
+import Searchbar from "../components/Searchbar";
 
 const FindPage = () => {
     return (
@@ -10,18 +11,13 @@ const FindPage = () => {
                 <div className='space-100px'></div>
                 <div className="small-title">GET INVOLVED</div>
                 <div className="greetings">| 만나요에서 새로운 만남을 찾아보세요.</div>
-                <div className="greetings">| 10개의 만남이 당신을 기다리고 있습니다!</div>
-
-                <div className='space-50px'></div>
+                <div className="greetings">| 10개의 만남이 당신을 기다리고 있어요!</div>
                 
+                <Searchbar/>
+                <div className='space-50px'></div>
+                    
                 <div className="list-wrapper">
                     <MeetingCard/>
-                    <MeetingCard/>
-                    <MeetingCard/>
-                    <MeetingCard/>
-                    <MeetingCard/>
-                    <MeetingCard/>
-                    <MeetingCard/>  
                 </div>
             </div>
             <Footer/>
